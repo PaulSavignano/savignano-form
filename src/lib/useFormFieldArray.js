4d616760c18d694c6d29e2253476b9b516e80c51
Paul Savignano <Paul.Savignano@gmail.com>
2019-12-30 15:35:42 -0800

    [useFormFieldArray] remove unused import

--- src/lib/useFormFieldArray.js
+++ src/lib/useFormFieldArray.js
@@ -1,4 +1,4 @@
-import { useCallback, useContext, useState, useEffect, useRef } from 'react'
+import { useContext, useState, useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 
 import FormContext from './FormContext'
